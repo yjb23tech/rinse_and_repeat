7da3e7cca504c8aaa9fb2cb5b38f89c163e14ec7
yjb23tech <yjb23tech@gmail.com>
2023-12-27 02:49:51 +0000

    Jupiter Eating His Children

--- daily_labs/spotify/App.jsx
+++ daily_labs/spotify/App.jsx
@@ -6,6 +6,16 @@ const App = () => {
     return (
         <div>
             <NavigationBar />
+            <Card 
+                song_cover_art="track_7_cover_art.png"
+                song_title="Devil In A New Dress"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Sophisticated Ignorance"
+            />
         </div>
     )
 }
