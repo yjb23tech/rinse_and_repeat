47e1ba0073ba52357a8a9dd7655310a096ffadec
yjb23tech <yjb23tech@gmail.com>
2023-12-27 02:30:22 +0000

    Die 4 Me

---
+++ daily_labs/spotify/App.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client"
+import NavigationBar from "./components/NavigationBar.jsx"
+import Card from "./components/Card.jsx"
+
+const App = () => {
+    return (
+        <div>
+            <NavigationBar />
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
+
---
+++ daily_labs/spotify/components/NavigationBar.jsx
@@ -0,0 +1,9 @@
+const NavigationBar = () => {
+    return (
+        <nav className="universal-navi">
+            <h3>Anstrum Labs</h3>
+        </nav>
+    )
+}
+
+export default NavigationBar;
